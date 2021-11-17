f653d90051a9fce113df8ac575b19798efe85a2e
LMika <lemika.pro@gmail.com>
2021-11-17 07:11:00 +0100

    Filter to access a specific article

---
+++ src/main/java/fr/mika/filter/ArticleFilter.java
@@ -0,0 +1,63 @@
+package fr.mika.filter;
+
+import java.io.IOException;
+
+import fr.mika.model.User;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class AdminFilter
+ */
+@WebFilter("/article.jsp")
+public class ArticleFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public ArticleFilter() {
+        
+    }
+    
+    @Override
+	public void init(FilterConfig fconfig) throws ServletException {
+		System.out.println("Article filter init");
+	}
+
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+		HttpSession session = request.getSession();
+		User user =  (User) session.getAttribute("user");
+		
+
+		System.out.println("Je suis là Article Filter");
+		
+		if (user==null) {
+			response.sendRedirect(request.getContextPath()+"/index.html");
+			return;
+		}
+		
+		chain.doFilter(req, res);
+	}
+	
+
+	@Override
+	public void destroy() {
+		System.out.println("Article filter destroy!");
+	}
+
+}
